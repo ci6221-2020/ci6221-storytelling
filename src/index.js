04a1c5a3681e899e93f90d2e5d9f68e4ace290c0
Robin <robinloh@u.nus.edu>
2020-11-04 00:16:07 +0800

    React setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const header = <h1>COVID-19 in Singapore</h1>
+const header = <h1>COVID-19 situation in Singapore</h1>
 
 ReactDOM.render(header, document.getElementById('root'));
